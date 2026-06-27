7c4dd46c54a9e4e5ceaad5719f2c24d97f01656d
alperentagmann <alperentagmann@gmail.com>
2026-06-28 02:01:34 +0300

    config: Mekan ismi, adres ve konum bilgileri Lidya Garden olarak guncellendi

--- lib/invite-config.ts
+++ lib/invite-config.ts
@@ -20,10 +20,10 @@ export const inviteConfig = {
   eventDate: "2026-07-12T13:00:00",
   dateLabel: "Pazar 12.07.26",
   timeLabel: "saat 13:00-16:00",
-  venueName: "Dadya Wedding 3",
-  venueAddress: "Seyri Sefa Restaurant (Karaağaç Sokak 6, Büyükçekmece, 34500, TR)",
+  venueName: "Lidya Garden",
+  venueAddress: "Karaağaç, Büyükçekmece Karaağaç Köyü Yolu No:1, 34488 Büyükçekmece/İstanbul",
   mapsUrl:
-    "https://www.google.com/maps/place/41%C2%B004'40.9%22N+28%C2%B034'59.0%22E/@41.0780258,28.5804768,17z/data=!3m1!4b1!4m4!3m3!8m2!3d41.0780258!4d28.5830517?hl=tr&entry=ttu&g_ep=EgoyMDI2MDYyNC4wIKXMDSoASAFQAw%3D%3D",
+    "https://www.google.com/maps/search/?api=1&query=Lidya+Garden+Karaagac+Buyukcekmece",
   receptionNote: "",
   rsvpDeadlineLabel: "10.05.26",
 } as const;
