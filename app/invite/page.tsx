12853f736a9ed77c6aa1301bbc86fe36f4bfb855
alperentagmann <alperentagmann@gmail.com>
2026-06-30 18:08:43 +0300

    style: Fotograf ikonunun dikey hizalamasi diger ikonlarla tam estetik simetri icin 2px yukari alindi

--- app/invite/page.tsx
+++ app/invite/page.tsx
@@ -63,7 +63,7 @@ function CheckIcon() {
 
 function CameraIcon() {
   return (
-    <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+    <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="-mt-[2px]">
       <path d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z"/>
       <circle cx="12" cy="13" r="3"/>
     </svg>
